6fe5c498fc09b4fd8af3b96adb209f61e8954986
tai-shis <questai.35@gmail.com>
2025-06-20 14:16:27 -0600

    Starting react based site

--- index.js
+++
@@ -1,9 +0,0 @@
-function copyTag() {
-    navigator.clipboard.writeText("bookychan")
-    alert("Tag copied to clipboard.")
-}
-
-function copyEmail() {
-    navigator.clipboard.writeText("tshis070@mtroyal.ca")
-    alert("Email copied to clipboard.")
-}
