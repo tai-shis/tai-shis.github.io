9721f240380f3029eaf862f2023204cbd353b866
tai-shis <questai.35@gmail.com>
2025-07-03 00:25:51 -0600

    tweaking router to fix rerouting too many times

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
@@ -10,12 +10,12 @@ function App() {
 			className='bg-background min-h-screen transpl-side-border-md pr-side-border-md pt-top-border-md 
                             lg:pl-side-border-full lg:pr-side-border-full lg:pt-top-border-full '
 		>
-			<BrowserRouter>
+			<Router>
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='*' element={<NoPage />} />
 				</Routes>
-			</BrowserRouter>
+			</Router>
 			<Footer />
 		</div>
 	);
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { execSync } from 'child_process';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { execSync } from "child_process";
 
-const commitDate = execSync('git log -1 --date=iso-strict --pretty=format:%cd')
-  .toString()
-  .trim();
+const commitDate = execSync("git log -1 --date=iso-strict --pretty=format:%cd")
+	.toString()
+	.trim();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  define: {
-    __COMMIT_DATE__: JSON.stringify(commitDate),
-  },
+	base: "/",
+	plugins: [react(), tailwindcss()],
+	define: {
+		__COMMIT_DATE__: JSON.stringify(commitDate),
+	},
 });
