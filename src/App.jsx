1ecf1e37fcc7eb810c7eb3584541839e1fd3e3bb
tai-shis <questai.35@gmail.com>
2025-07-04 20:05:25 -0600

    I forgot to import

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
 import Projects from "./pages/Projects.jsx";
+import Stuff from "./pages/Stuff.jsx";
 import NoPage from "./pages/NoPage.jsx";
 
 function App() {
