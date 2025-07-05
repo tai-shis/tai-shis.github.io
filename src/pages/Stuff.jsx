7a0ac885c7e452764a619fe95fe3d2410098e7fa
tai-shis <questai.35@gmail.com>
2025-07-04 19:59:39 -0600

    thats what i get for being lazy

--- src/pages/Stuff.jsx
+++ src/pages/Stuff.jsx
@@ -1,4 +1,4 @@
-function Projects() {
+function Stuff() {
 	return (
 		<div className='bg-element-bg border-2 border-border rounded-global p-6 m-element shadow-test h-full'>
 			<div className='text-default-text font-global text-xl'>
@@ -8,4 +8,4 @@ function Projects() {
 	);
 }
 
-export default Projects;
+export default Stuff;
