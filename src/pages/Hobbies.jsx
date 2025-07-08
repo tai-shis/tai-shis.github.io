5aaa3f15ce2d43415a440f98d0490c9993a46920
tai-shis <questai.35@gmail.com>
2025-07-08 00:11:51 -0600

    added other boards, have to fix everything shrinking on /hobbies

--- src/pages/Hobbies.jsx
+++ src/pages/Hobbies.jsx
@@ -1,6 +1,13 @@
 import analogDreams from "../assets/analogDreams.jpg";
+import dreamingGirl from "../assets/dreamingGirl.jpg";
+import dreamingGirlBack from "../assets/dreamingGirlBack.jpg";
+import yellow from "../assets/yellow.jpg";
+
+import { useState } from "react";
 
 function Hobbies() {
+	const [dgHover, setdgHover] = useState(false);
+
 	return (
 		<>
 			<div className='bg-element-bg border-border border-2 rounded-global shadow-test h-min m-element p-element flex-row flex'>
@@ -50,10 +57,30 @@ function Hobbies() {
 						This was my daily driver for quite some time and,
 						especially on the left hand side, you can see the
 						classic
-						<i> GMK Shine</i>&trade; on the keycaps. Probably one of
-						my favourite boards visually, with the whole vaporwave
-						aesthetic going on.
+						<i> GMK Shine</i>&trade; on the keycaps. The vaporwave
+						aesthetic make it probably one of my favourite boards
+						visually.
 					</p>
+				</div>
+			</div>
+			<div className='flex flex-row'>
+				<div className='bg-element-bg border-border border-2 rounded-global shadow-test m-element p-element w-2/3 h-144 block'>
+					<img
+						src={dgHover ? dreamingGirlBack : dreamingGirl}
+						alt='Dreaming Girl'
+						onMouseEnter={() => setdgHover(true)}
+						onMouseLeave={() => setdgHover(false)}
+						className='object-cover w-full h-96 border-2 border-element-bg rounded-global bg-element-bg ease-in-out transition duration-150
+                                   hover:shadow-test hover:border-background hover:translate-x-1.5 hover:-translate-y-1.5'
+					/>
+				</div>
+				<div className='bg-element-bg border-border border-2 rounded-global shadow-test m-element p-element w-1/3 h-144 block'>
+					<img
+						src={yellow}
+						alt='Yellow'
+						className='object-cover w-full h-max border-2 border-element-bg rounded-global bg-element-bg ease-in-out transition duration-150
+                                   hover:shadow-test hover:border-background hover:translate-x-1.5 hover:-translate-y-1.5'
+					/>
 				</div>
 			</div>
 		</>
