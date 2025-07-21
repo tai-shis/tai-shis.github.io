4eada6f7683139005920458999802b5feff8cf00
tai-shis <questai.35@gmail.com>
2025-07-21 17:16:18 -0600

    silly motion css mistake

--- src/pages/Hobbies.jsx
+++ src/pages/Hobbies.jsx
@@ -5,21 +5,17 @@ import { Link } from "react-router-dom";
 function Hobbies() {
 	return (
 		<div className='min-w-full mt-6 mb-6 mx-auto lg:min-w-5xl flex lg:flex-row flex-col'>
-			<motion.div
-				className='element-primary p-6 flex lg:w-1/2'
-				initial={{ scale: 0.5 }}
-				animate={{ scale: 1 }}
-			>
-				<Link to='/hobbies/keyboards'>Keyboards</Link>
-			</motion.div>
+			<div className='element-primary p-6 flex lg:w-1/2'>
+				<motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
+					<Link to='/hobbies/keyboards'>Keyboards</Link>
+				</motion.div>
+			</div>
 
-			<motion.div
-				className='element-primary p-6 flex lg:w-1/2'
-				initial={{ scale: 0.5 }}
-				animate={{ scale: 1 }}
-			>
-				<p>This page is a work in progress</p>
-			</motion.div>
+			<div className='element-primary p-6 flex lg:w-1/2'>
+				<motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
+					<p>This page is a work in progress</p>
+				</motion.div>
+			</div>
 		</div>
 	);
 }
