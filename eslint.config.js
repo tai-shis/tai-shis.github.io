3e427b0536b3ae451428f0d1407c62cf4dae7320
tai-shis <questai.35@gmail.com>
2025-10-10 21:22:32 -0600

    bleh

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,23 @@
-import js from "@eslint/js";
-import globals from "globals";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-	{ ignores: ["dist/", "docs/"] },
-	{
-		files: ["**/*.{js,jsx}"],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
-			parserOptions: {
-				ecmaVersion: "latest",
-				ecmaFeatures: { jsx: true },
-				sourceType: "module",
-			},
-		},
-		plugins: {
-			react,
-			"react-hooks": reactHooks,
-			"react-refresh": reactRefresh,
-		},
-		rules: {
-			...js.configs.recommended.rules,
-			...react.configs.recommended.rules,
-			...reactHooks.configs.recommended.rules,
-			"react/jsx-uses-vars": "error",
-			"no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-			"react-refresh/only-export-components": [
-				"warn",
-				{ allowConstantExport: true },
-			],
-		},
-	},
-];
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
--- src/pages/NoPage.jsx
+++
@@ -1,9 +0,0 @@
-function NoPage() {
-	return (
-		<>
-			<h1>Bruh</h1>
-		</>
-	);
-}
-
-export default NoPage;
